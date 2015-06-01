0fdab01b0cd6dd733b8f43226937721f9eb0b61d
Jason Storey <jason.andrew.storey@gmail.com>
2015-06-01 02:27:44 +0100

    Refactor all the things and add UI for switching

--- behaviours.js
+++ behaviours.js
@@ -1,42 +1,86 @@
-var NUMBER_OF_MOVERS = 100;
-var movers = [];
+function Behaviours() {
+    this.movement = MOVEMENTS.weave;
+    this.movers = [];
+}
 
-var BEHAVIOURS = {
-    reset: function() {
-        this.radius = 1;
-        this.theta = random(TWO_PI);
-        this.drag = 0.2;
-        this.vx = 0;
-        this.vy = 0;
+Behaviours.prototype = {
+    init: function() {
+        var self = this;
+        var NUMBER_OF_MOVERS = 100;
+
+        Sketch.create({
+            setup: function () {
+                for (var j = 0; j < NUMBER_OF_MOVERS; j++) {
+                    self.movers.push(new Mover(random(this.width), random(this.height), self.movement));
+                }
+            },
+            update: function () {
+                self.movers.forEach(function (mover) {
+                    mover.update(this);
+                }.bind(this));
+            },
+            draw: function () {
+                self.movers.forEach(function (mover) {
+                    mover.draw(this);
+                }.bind(this));
+            }
+        });
     },
+    reset: function() {
+        this.movers.forEach(function (mover) {
+            mover.reset();
+        });
+    },
+    update: function() {
+        this.movers.forEach(function (mover) {
+            mover.reset();
+            mover.setMovement(this.movement)
+        }.bind(this));
+    }
+};
+
+var MOVEMENTS = {
     weave: function() {
-        this.theta += random(-0.5, 0.5);
+        this.theta += random(-0.3, 0.3);
         this.vx += sin(this.theta);
         this.vy += cos(this.theta);
         this.vx *= 1 - this.drag;
         this.vy *= 1 - this.drag;
     },
     grow: function() {
-        this.radius *= 1.01;
+        MOVEMENTS.weave.call(this);
+        this.radius *= random(1, 1.03);
     },
     flutter: function() {
         this.drag = 0.1;
         this.radius = random(10);
         this.theta += random(0.15);
         this.vx += sin(this.theta);
-        this.vy += (cos(this.theta) / 2) - random(0.3);
+        this.vy += (cos(this.theta) / 3) - random(0.3);
         this.vx *= 1 - this.drag;
         this.vy *= 1 - this.drag;
     }
 };
 
-function Mover(x, y) {
+function Mover(x, y, movement) {
     this.x = x;
     this.y = y;
-    BEHAVIOURS.reset.call(this);
+    this.movement = movement;
+    this.reset();
 }
 
 Mover.prototype = {
+    reset: function() {
+        this.radius = 3;
+        this.theta = random(TWO_PI);
+        this.drag = 0.2;
+        this.vx = 0;
+        this.vy = 0;
+    },
+    setMovement: function(movement) {
+        this.reset();
+        this.movement = movement;
+    },
     update: function(ctx) {
         this.x += this.vx;
         this.y += this.vy;
@@ -53,7 +97,7 @@ Mover.prototype = {
             this.y = ctx.height + this.radius;
         }
 
-        BEHAVIOURS.flutter.call(this);
+        this.movement();
     },
     draw: function(ctx) {
         ctx.beginPath();
@@ -63,20 +107,17 @@ Mover.prototype = {
     }
 };
 
-Sketch.create({
-    setup: function() {
-        for(var j = 0; j < NUMBER_OF_MOVERS; j++) {
-            movers.push(new Mover(random(this.width), random(this.height)));
-        }
-    },
-    update: function() {
-        movers.forEach(function(mover) {
-            mover.update(this);
-        }.bind(this));
-    },
-    draw: function() {
-        movers.forEach(function(mover) {
-            mover.draw(this);
-        }.bind(this));
-    }
+var behaviourConfig = {
+    movement: 'weave'
+};
+
+var behaviours = new Behaviours();
+behaviours.init();
+
+var gui = new dat.GUI();
+var movementController = gui.add(behaviourConfig, 'movement', ['weave', 'flutter', 'grow']);
+
+movementController.onChange(function(value) {
+    behaviours.movement = MOVEMENTS[value];
+    behaviours.update();
 });
